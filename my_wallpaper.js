c56d5e58e6eb105d4224e3a550e9a972bbf20fac
LunkKitchen <lukejkitching@icloud.com>
2024-07-31 18:45:39 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,12 +1,16 @@
 //your parameter variables go here!
 let rect_width  = 20;
 let rect_height = 20;
+var ellipseHeight = 200;
+var eyeLength = 50;
+var eyeWidth = 10;
+var eyeColor = random(255);
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -15,9 +19,19 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(240); //light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  strokeWeight(2);
+  stroke(0,0,0);
+  fill(random(255),random(255),random(255));
+  ellipse(100,100,ellipseHeight,ellipseHeight);
+
+  fill(random(255));
+  ellipse(75,75,eyeWidth,eyeLength);
+  ellipse(125,75,eyeWidth,eyeLength);
+  
+  fill(random(255));
+  curve(75, 75, 50, 150, 150, 150, 125, 75);
 }
