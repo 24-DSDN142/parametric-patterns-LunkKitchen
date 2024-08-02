2209c3f2b217e97fff1c6b1627ea4183b84fdc9a
LunkKitchen <lukejkitching@icloud.com>
2024-08-02 13:33:23 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,16 +1,16 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
+let headX = 100;
+let headY = 100;
+var headScale = 1;
 var ellipseHeight = 200;
 var eyeLength = 50;
 var eyeWidth = 10;
 var eyeColor = random(255);
 
-
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(false); //set this to false when you're ready to print
+  pWallpaper.show_guide(true); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -23,15 +23,19 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
+  DrawSmiley();
+}
+
+function DrawSmiley(){
   strokeWeight(2);
   stroke(0,0,0);
   fill(random(255),random(255),random(255));
-  ellipse(100,100,ellipseHeight,ellipseHeight);
+  ellipse(headX,headY,headScale*ellipseHeight,headScale*ellipseHeight);
 
   fill(random(255));
-  ellipse(75,75,eyeWidth,eyeLength);
-  ellipse(125,75,eyeWidth,eyeLength);
+  ellipse(75,75,headScale*eyeWidth,headScale*eyeLength);
+  ellipse(125,75,headScale*eyeWidth,headScale*eyeLength);
   
   fill(random(255));
   curve(75, 75, 50, 150, 150, 150, 125, 75);
-}
+};
